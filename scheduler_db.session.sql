3902fdf80ac87d676c2039660cdbaa9cf56f87ba
kyareema01 <kyareema@gmail.com>
2026-09-19 15:31:41 +0100

    Permanently remove node_modules from remote repository

--- scheduler_db.session.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE appointments (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  customer_name VARCHAR(100),
-  start_time BIGINT,
-  end_time BIGINT,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
